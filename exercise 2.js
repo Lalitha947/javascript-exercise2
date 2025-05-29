b410bffa6492222adcd45cd47c437fbc8e83f77c
Lalitha947 <lalithancana415@gmail.com>
2025-05-29 15:59:08 +0200

    newfile

---
+++ exercise 2.js
@@ -0,0 +1,13 @@
+// TODO: Create a variable called number1 and store any number in it
+number1 = 25
+// TODO: Create a variable called number2 and store a different number in it
+number2 = 10
+// TODO: Convert number1 to a string and store it in a new variable
+number_1 = String(number1)
+// TODO: Add number1 and number2 and store the result in a variable called sum
+sum = number1 + number2
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+product = number1 * number2
+// TODO: Print all results to the console
+console.log(product);
+console.log(sum);
